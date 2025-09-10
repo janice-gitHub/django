b5119598770e1b211fd606d64a383e39eb348303
unknown <janicelu0623@gmail.com>
2025-09-10 20:15:46 +0800

    完成首頁

--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse
 import json
+from .models import Todo
+
+
+def todolist(request):
+    todos = Todo.objects.all()
+
+    return render(request, "todo/todolist.html", {"todos": todos})
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Hello django!</h1>")
 
-def books(request):
-    my_books={1:"Pythyon",2:"Java",3:"C# book"}
 
-    return HttpResponse(json.dumps(my_books), 
-                        content_type="application/json")
+def books(request):
+    my_books = {1: "Pythyon", 2: "Java", 3: "C# book"}
+
+    return HttpResponse(json.dumps(my_books), content_type="application/json")
--- todolist/urls.py
+++ todolist/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from todo import views
 
 
 urlpatterns = [
-    path('books/', views.books),
-    path('', views.index),
-    path('admin/', admin.site.urls),
+    path("books/", views.books),
+    path("", views.todolist),
+    path("admin/", admin.site.urls),
 ]
